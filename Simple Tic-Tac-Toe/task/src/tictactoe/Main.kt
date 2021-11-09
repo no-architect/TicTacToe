7979de2c99ffbf4fc02d4e62669c4e7cce3dd454
no-architect <alexandrneberekutin@gmail.com>
2021-11-09 14:09:09 +0300

    Initial commit

--- Simple Tic-Tac-Toe/task/src/tictactoe/Main.kt
+++ Simple Tic-Tac-Toe/task/src/tictactoe/Main.kt
@@ -2,4 +2,15 @@ package tictactoe
 
 fun main() {
     // write your code here
+    val curState = readLine()!!
+    if (curState.length == 9) {
+        print("---------\n| ")
+        for (cur in 0..curState.lastIndex){
+            print(curState[cur] + " ")
+            if (((cur + 1) % 3 == 0) && (cur > 0) && (cur < 8)){
+                print("|\n| ")
+            }
+        }
+        println("|\n---------")
+    } else println("no correct")
 }
