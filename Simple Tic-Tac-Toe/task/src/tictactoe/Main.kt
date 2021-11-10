74e577a4462abba06a75106369f3a8bfb8b5d016
no-architect <alexandrneberekutin@gmail.com>
2021-11-10 15:44:44 +0300

    Stage 4 pass "First move"

--- Simple Tic-Tac-Toe/task/src/tictactoe/Main.kt
+++ Simple Tic-Tac-Toe/task/src/tictactoe/Main.kt
@@ -1,7 +1,6 @@
 package tictactoe
 
-import java.lang.Math.abs
-
+// function find "XXX" on horizontal/vertical/diagonal
 fun winsX( str: String): Boolean {
 
     if (Regex("(XXX.{6})|(...XXX...)|(.{6}XXX)").containsMatchIn(str) ||
@@ -11,6 +10,8 @@ fun winsX( str: String): Boolean {
     }
     return false
 }
+
+// function find "OOO" on horizontal/vertical/diagonal
 fun winsO( str: String): Boolean {
 
     if (Regex("(OOO.{6})|(...OOO...)|(.{6}OOO)").containsMatchIn(str) ||
@@ -20,11 +21,13 @@ fun winsO( str: String): Boolean {
     }
     return false
 }
+
+// function return result current state
 fun resStr( str: String): String {
     val countX = Regex("X").findAll(str).count()
     val countO = Regex("O").findAll(str).count()
     val countE = Regex("_").findAll(str).count()
-    if (abs(countX - countO) > 1) { return "Impossible " }
+    if (kotlin.math.abs(countX - countO) > 1) { return "Impossible " }
     if (winsO(str) && winsX(str)) { return "Impossible" }
     if (winsX(str)) { return "X wins" }
     if (winsO(str)) { return "O wins" }
@@ -32,18 +35,60 @@ fun resStr( str: String): String {
     return "Game not finished"
 }
 
-fun main() {
-    // write your code here
-    val curState = readLine()!!
-    if (curState.length == 9) {
+// function print Cell if format
+fun printCell(str: String) {
+    if (str.length == 9) {
         print("---------\n| ")
-        for (cur in 0..curState.lastIndex){
-            print(curState[cur] + " ")
+        for (cur in 0..str.lastIndex){
+            print(str[cur] + " ")
             if (((cur + 1) % 3 == 0) && (cur > 0) && (cur < 8)){
                 print("|\n| ")
             }
         }
         println("|\n---------")
     } else println("no correct")
-    println(resStr(curState))
+}
+
+// function conver coordinat to index
+fun convertCoordinatToIndex(cell: String): Int {
+    return (cell.replace(" ", "").toInt() / 10 -1) * 3 + cell.replace(" ", "").toInt() % 10 -1
+}
+
+// function return result verification can be changed cell.xy in cells
+fun canBeChangedCell(cells: String, xy: String): Boolean {
+    val inCell = xy.replace(" ", "")
+    // verification input non-numeric two symbols
+    if (inCell.length != 2 && inCell.toIntOrNull() == null) { println("You should enter numbers!"); return false }
+    if (Regex("[1-3]").findAll(inCell).count() != 2 || inCell.length != 2) { println("Coordinates should be from 1 to 3!"); return false }
+    if (cells[convertCoordinatToIndex(inCell)] != '_') {
+        println("This cell is occupied! Choose another one!")
+        return false }
+    return true
+}
+
+//function return cells after changed
+fun changeCell(cells: String, cell: String): String {
+    val inCell = cell.replace(" ", "")
+    println("proverka")
+    if (canBeChangedCell(cells, inCell)) {
+        println("proverka pass")
+        return cells.replaceRange(convertCoordinatToIndex(inCell), convertCoordinatToIndex(inCell) + 1, "X")
+    }
+    return cells
+}
+
+fun main() {
+    print("Enter cells:")
+    val curState = readLine()!!
+
+    printCell(curState)
+    print("Enter the coordinates:")
+    var curCell = readLine()!!
+    while (!canBeChangedCell(curState, curCell)) {
+        print("Enter the coordinates:")
+        curCell = readLine()!!
+    }
+    println(changeCell(curState, curCell))
+    printCell(changeCell(curState, curCell))
+
 }
